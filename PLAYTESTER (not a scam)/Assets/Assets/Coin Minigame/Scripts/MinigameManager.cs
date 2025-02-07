d5dc07f42e5bb03e64feba12ed04794016301593
michelledeng2688 <mfdeng@ualberta.ca>
2025-02-06 20:48:44 -0700

    Added coin runner minigame

---
+++ PLAYTESTER (not a scam)/Assets/Assets/Coin Minigame/Scripts/MinigameManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    GameObject UIController;
+
+
+    // Manages glitches 
+    public float glitchFreq = 0.5f; // Set based on the day and situation
+    public float glitchWaitTime = 1f;
+    public float gameDur = 30f;
+
+    // Timer management 
+    public TMP_Text timerText;
+    private bool isGameOver = false;
+
+    
+    // bg1, bg2 
+    // Good spawnable
+    // Audio
+    // For now, just player color will change
+    public SpriteRenderer playerRenderer;
+    public Color glitchPlayerColor = new Color(1f, 0f, 1f, 1f); // aMgenta
+    private Color normalPlayerColor;
+
+    void Start()
+    {
+        UIController = GameObject.Find("UI Controller");
+
+        normalPlayerColor = playerRenderer.color; // Set starting color as player's uncorrupted color
+
+        StartCoroutine(GlitchCheckRoutine()); 
+        StartCoroutine(StartMinigameTimer());
+    }
+
+    IEnumerator GlitchCheckRoutine()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(glitchWaitTime); // Check every second
+
+            if (glitchFreq > 0 && Random.value < glitchFreq) // If we get a random value less than the glitch frequency, the glitches will occur
+            {
+                StartCoroutine(ActivateGlitch()); 
+            }
+        }
+    }
+
+    IEnumerator ActivateGlitch()
+    {
+        // Apply glitch effects
+        playerRenderer.color = glitchPlayerColor;
+      
+        yield return new WaitForSeconds(1f); // Glitch lasts 1 second
+
+        // Revert to normal if the glitch frequency isn't 100%
+        if (glitchFreq != 1)
+        {
+           playerRenderer.color = normalPlayerColor;
+        }
+      
+    }
+
+    IEnumerator StartMinigameTimer()
+    {
+        float timer = gameDur; // Set timer
+
+        while (timer > 0 && !isGameOver)
+        {
+            timer -= Time.deltaTime; // Update timer
+            timerText.text = "Time: " + Mathf.RoundToInt(timer); // Display timer (rounded)
+            yield return null;
+        }
+
+        GameOver(); // When time is up, call game over function
+    }
+
+    void GameOver()
+    {
+        isGameOver = true;
+        timerText.text = "Time's Up!";
+        Time.timeScale = 0f; // Stop everything
+        UIController.GetComponent<ComputerUIScript>().GoToPosition(new Vector3(90, 35, -10));
+    }
+}
+
---
+++ PLAYTESTER (not a scam)/Assets/Assets/Coin Minigame/Scripts/spawnMg.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnMg : MonoBehaviour
+{
+    public GameObject spawnable;
+    public Transform[] spawnPos;
+    public float spawnInterval = 1.5f;
+    public float speed = 2f;
+    Color colorOne = new Color(1f, 1f, 0f, 1f); // Yellow
+    Color colorTwo = new Color(0.5f, 0f, 0.5f, 1f); // Purple
+
+    void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    IEnumerator SpawnObjects()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval); // Make spawnables every x seconds
+
+            int slotOne = Random.Range(0, spawnPos.Length);
+            int slotTwo;
+            do
+            {
+                slotTwo = Random.Range(0, spawnPos.Length);
+            } while (slotTwo == slotOne); // Ensure the slots are different
+
+            Spawn(spawnPos[slotOne], colorOne); // This will be the good coin
+            Spawn(spawnPos[slotTwo], colorTwo); // This will be the bad coin
+        }
+    }
+
+    void Spawn(Transform spawnPos, Color color)
+    {
+        GameObject newSpawnable = Instantiate(spawnable, spawnPos.position, Quaternion.identity); // Make the spawnable
+
+        SpriteRenderer spriteRenderer = newSpawnable.GetComponent<SpriteRenderer>(); // Access the spawnable's color
+        spriteRenderer.color = color; // Change the spawnable's color
+    }
+}
