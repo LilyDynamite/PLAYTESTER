39f132e30f3272ab617542038b9f27e25c3233f1
Neila <159399995+Neil-Jayyn@users.noreply.github.com>
2025-02-07 20:59:11 -0700

    added cupcake minigame

---
+++ PLAYTESTER (not a scam)/Assets/Assets/Cupcake Minigame/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject person;
+
+    [SerializeField] private int numEnemies;
+    private int currentNumEnemies;
+    private float waitTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn enemies after each spawn wait time with a max amount of people
+        waitTime-=Time.deltaTime;
+        if (waitTime<=0 && currentNumEnemies<numEnemies){
+            Instantiate(person,transform.position,Quaternion.identity);
+            setSpawnTime();
+            currentNumEnemies++;
+        }
+    }
+
+    void setSpawnTime(){
+        waitTime=Random.Range(0.5f,3.0f);
+    }
+}
---
+++ PLAYTESTER (not a scam)/Assets/Assets/Cupcake Minigame/Scripts/enemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{ 
+    public float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float minSpeed;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed=Random.Range(minSpeed,maxSpeed); //randomize speed of people
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if it hits the walls of the game, move other way
+        if (other.gameObject.tag == "Boundary")
+        {
+            speed *= -1;
+        }
+    }
+}
