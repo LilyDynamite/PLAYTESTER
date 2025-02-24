8f7b53d9a6bac85391029d0218a08dd97b9c5fe1
Neila <159399995+Neil-Jayyn@users.noreply.github.com>
2025-02-23 20:03:02 -0700

    cupcake person animation

--- PLAYTESTER (not a scam)/Assets/Assets/Cupcake Minigame/Scripts/enemyMovement.cs
+++ PLAYTESTER (not a scam)/Assets/Assets/Cupcake Minigame/Scripts/enemyMovement.cs
@@ -7,10 +7,13 @@ public class enemyMovement : MonoBehaviour
     public float speed;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float minSpeed;
+
+    SpriteRenderer spriteRenderer;
  
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         speed=Random.Range(minSpeed,maxSpeed); //randomize speed of people
   
     }
@@ -24,9 +27,18 @@ public class enemyMovement : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         //if it hits the walls of the game, move other way
-        if (other.gameObject.tag == "Boundary")
+        if (other.gameObject.tag == "LBoundary")
         {
             speed *= -1;
+            spriteRenderer.flipX= false;
+
+        }
+
+        if (other.gameObject.tag == "RBoundary")
+        {
+            speed *= -1;
+            spriteRenderer.flipX = true;
+
         }
     }
 }
